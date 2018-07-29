9e52fcf02c022366d39396b44bdc6272c5eb957c
Gaurav Kumar <gauravsrivastav04@gmail.com>
2018-07-29 21:19:40 +0530

    Change in build.gradle and PersonRestBase class

--- src/test/java/com/startwithjava/springwebfluxcontract/cdc/PersonRestBase.java
+++ src/test/java/com/startwithjava/springwebfluxcontract/cdc/PersonRestBase.java
@@ -1,18 +1,22 @@
 package com.startwithjava.springwebfluxcontract.cdc;
+import com.startwithjava.springwebfluxcontract.SpringWebfluxContractApplication;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import io.restassured.RestAssured;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment=SpringBootTest.WebEnvironment.RANDOM_PORT,classes = SpringWebfluxContractApplication.class)
 public abstract class PersonRestBase {
+	@LocalServerPort
+	int port;
 
 	@Before
 	public void setup() {
-		RestAssured.baseURI = "http://localhost:9500";
+		RestAssured.baseURI = "http://localhost:" + this.port;
 	}
 	
 }
